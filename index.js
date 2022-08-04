debf5fd3ac390504e53dc3e10bcefa62282b6c11
Pury <szwzjx@163.com>
2022-08-05 07:58:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+// main.js
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // preload: path.join(__dirname, 'preload.js')
+    },
+    icon: 'assets/logo.ico'  // 窗口小图标
+  })
+
+  mainWindow.loadURL('https://cpury.com/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
